638ba25bd0f685602318ae6c8dd7220c07216722
liping <366147540@qq.com>
2017-03-31 21:14:26 +0800

    s

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 var express=require('express');
 var app=express();
+var ejs = require('ejs');
 
-app.use(express.static('app/html'));
-app.use(express.static('app'));
+app.use(express.static('app/static'));
+app.use(express.static('app/static/html'));
+
+
+app.engine('html',ejs.__express);
+app.set('views','./app/views');
+app.set('view engine','html');
+
+
+app.get('/demo/babel',function(req,res){
+	console.log('访问了babel');
+	
+});
 
 app.get('/',function(req,res){
-	console.log('访问了/');
-	// res.send("完成了访问");
-	res.download("LICENSE");
+	res.render('index',{title:"金飞的个人网站"});
+});
+
+app.get('/views', function (req, res) {
+  res.render('index', { title: 'Hey', message: 'Hello there!'});
+});
+
+app.get('/dt/babel',function(req,res){
+	res.render('demoandtest/babel');
 });
 
 var server=app.listen(8051,function(){
